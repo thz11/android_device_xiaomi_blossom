f6e8c8e4a232e5589f4274908011638f5a683f1b
thz <diasjuremathiago@gmail.com>
2025-04-04 18:06:28 +0000

    blossom: enable JAVA optimizations
    imported from AOSPA bruh

--- device.mk
+++ device.mk
@@ -437,6 +437,10 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Java Optimizations
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
